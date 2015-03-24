b000fe1ad36b901c4a13e332dcfe54219c3e38ad
valkjsaaa <jackieyang51@gmail.com>
2015-03-24 22:07:00 +0800

    added rippler effect

--- interface.js
+++ interface.js
@@ -1,6 +1,45 @@
 var imgurl
 
+$(document).ready(function() {
+  $(".rippler").rippler({
+    effectClass      :  'rippler-effect'
+    ,effectSize      :  16      // Default size (width & height)
+    ,addElement      :  'div'   // e.g. 'svg'(feature)
+    ,duration        :  400
+  });
+});
 
+$('#likeButton').on('click', function() {
+    // var $btn = $(this).button('loading')
+    console.log("Like button clicked.")
+    $.ajax({
+        type: 'GET',
+        url: 'http://photo.yangjunrui.com/like',
+        data: {
+            url: imgurl
+        },
+        dataType: 'json',
+        complete: function(XMLHttpRequest, textStatus) {
+            // $btn.button('reset')
+        }
+    });
+})
+
+$('#dislikeButton').on('click', function() {
+    // var $btn = $(this).button('loading')
+    console.log("Like button clicked.")
+    $.ajax({
+        type: 'GET',
+        url: 'http://photo.yangjunrui.com/dislike',
+        data: {
+            url: imgurl
+        },
+        dataType: 'json',
+        complete: function(XMLHttpRequest, textStatus) {
+            // $btn.button('reset')
+        }
+    });
+})
 
 function getRandomNumber(l, r) {
     return l + Math.floor((Math.random() * (r - l)));
@@ -40,40 +79,9 @@ function getNextImg(color) {
             if (xmlHttp.readyState === 4) {
                 if (xmlHttp.status === 200) {
                     imgurl = xmlHttp.responseText;
+                    $('#likeButton').removeClass("hidden");
+                    $('#dislikeButton').removeClass("hidden");
                     document.body.style.backgroundImage = "url('" + imgurl + "')";
-                    document.getElementById("rate_div_id").innerHTML = "\n    <div class=\"btn-group\" role=\"group\" aria-label=\"...\">\n        <a id=\"likeButton\" class=\"btn btn-link rippler rippler-bs-primary\"><span class=\"glyphicon glyphicon-thumbs-up\" aria-hidden=\"true\"></span></button>\n        <a id=\"dislikeButton\" class=\"btn btn-link rippler rippler-bs-primary\"><span class=\"glyphicon glyphicon-thumbs-down\" aria-hidden=\"true\"></span></button>\n    </div>"
-                    $('#likeButton').on('click', function() {
-                        // var $btn = $(this).button('loading')
-                        console.log("Like button clicked.")
-                        $.ajax({
-                            type: 'GET',
-                            url: 'http://photo.yangjunrui.com/like',
-                            data: {
-                                url: imgurl
-                            },
-                            dataType: 'json',
-                            complete: function(XMLHttpRequest, textStatus) {
-                                $btn.button('reset')
-                            }
-                        });
-                    })
-
-                    $('#dislikeButton').on('click', function() {
-                        // var $btn = $(this).button('loading')
-                        console.log("Like button clicked.")
-                        $.ajax({
-                            type: 'GET',
-                            url: 'http://photo.yangjunrui.com/dislike',
-                            data: {
-                                url: imgurl
-                            },
-                            dataType: 'json',
-                            complete: function(XMLHttpRequest, textStatus) {
-                                $btn.button('reset')
-                            }
-                        });
-                    })
-
                 } else {
                     // alert("xmlHttp.status ==" + xmlHttp.status);
                 }
